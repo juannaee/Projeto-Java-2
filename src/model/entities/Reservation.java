3d0a7fb6a62d2b7d013eaed075d2f3018cebd079
Juan Guilherme <juangsilvalemos@gmail.com>
2023-02-08 15:08:48 -0300

    Alterações feita no trabalho

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,52 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+	private Integer numberRoom;
+	private Date checkIn;
+	private Date checkOut;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	public Reservation(Integer numberroom, Date checkIn, Date checkOut) {
+		this.numberRoom = numberroom;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getNumberroom() {
+		return numberRoom;
+	}
+
+	public void setNumberroom(Integer numberroom) {
+		this.numberRoom = numberroom;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MICROSECONDS);
+	}
+
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	
+	@Override 
+	public String toString() {
+		return "Room " + numberRoom  + ", check-in: "; 
+	}
+
+}
